5c430fc24ae9d3f5dad65607c39e4415f20091ee
Larissa Sales <larissa.s.sales@outlook.com>
2019-05-07 19:09:54 -0300

    Modified gitignore

--- Backend/src/main/java/com/stefanini/projeto/repository/ParentRepository.java
+++ Backend/src/main/java/com/stefanini/projeto/repository/ParentRepository.java
@@ -1,9 +1,12 @@
 package com.stefanini.projeto.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.stefanini.projeto.model.Parent;
 
 public interface ParentRepository extends CrudRepository<Parent, Long>{
-
+	
+	List<Parent> findByNome(String nome);
 }
